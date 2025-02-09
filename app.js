8995fc401af502638164104b2a4c3de0b867df7a
Natasha Ribeiro <70410978+natasharhayane@users.noreply.github.com>
2025-02-08 23:33:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
 let amigos = [];
 let numerosSorteados = [];
-let tamanhoArray;
 
-// Função para adicionar um amigo à lista
 function adicionarAmigo() {
-    let inputAmigo = document.getElementById('amigo');
-    let nomeAmigo = inputAmigo.value.trim();
+    const inputAmigo = document.getElementById('amigo');
+    const nomeAmigo = inputAmigo.value.trim();
 
     if (!nomeAmigo) {
         alert('Por favor, insira um nome.');
@@ -17,45 +17,39 @@ function adicionarAmigo() {
     inputAmigo.value = '';
 }
 
-// Função para atualizar a exibição da lista de amigos
 function atualizarListaDeAmigos() {
-    let listaExibicao = document.getElementById('listaAmigos');
+    const listaExibicao = document.getElementById('listaAmigos');
     listaExibicao.innerHTML = '';
 
-    amigos.forEach(amigo => {
-        let li = document.createElement('li');
+    amigos.forEach((amigo) => {
+        const li = document.createElement('li');
         li.textContent = amigo;
         listaExibicao.appendChild(li);
     });
 }
 
-// Função para sortear um amigo
+
 function sortearAmigo() {
     if (amigos.length === 0) {
         alert('Por favor, adicione amigos antes de sortear.');
         return;
     }
-    let numeroSorteado = gerarNumeroAleatorio();
-    exibirAmigoSorteado(numeroSorteado);
+
+    if (numerosSorteados.length >= amigos.length) {
+        numerosSorteados.splice(0);
+      }
+      
+      let numeroSorteado;
+      do{
+          numeroSorteado = Math.floor(Math.random() * amigos.length);
+      } while(numerosSorteados.includes(numeroSorteado))
+      
+      numerosSorteados.push(numeroSorteado);
+      
+      exibirAmigoSorteado(numeroSorteado);
 }
 
-// Função para gerar um número aleatório
-function gerarNumeroAleatorio() {
-  
-    tamanhoArray = amigos.length;
-    let novoNumeroGerado;
-    do{
-      novoNumeroGerado = parseInt(Math.random() * tamanhoArray);
-    }while(numerosSorteados.includes(novoNumeroGerado))
-    
-    numerosSorteados.push(novoNumeroGerado);
-    return novoNumeroGerado;
-
-}
-
-// Função para exibir o amigo sorteado
 function exibirAmigoSorteado(index) {
-    let sorteado = document.getElementById('resultado');
-    sorteado.innerHTML = `<li>${amigos[index]}</li>`; 
+  const resultadoElemento= document.getElementById('resultado');
+  resultadoElemento.innerHTML=`<li>${amigos[index]}</li>`;
 }
-
